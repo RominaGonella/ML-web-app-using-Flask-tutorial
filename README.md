ac052996d7ed6a40c8a1c8ac2cf6f0fbc8d89783
RominaGonella <rominagonfur@gmail.com>
2022-08-17 20:28:28 +0000

    versión final

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Template for Machine Learning projects
+# Resumen del proceso
+
+1. Se crea app para el modelo coffee_model.pkl usando Flask un formulario básico.
+
+2. El modelo toma valores de las siguientes variables:
+
+* country, una de las siguientes opciones: Brazil, Colombia, Guatemala, Mexico, Taiwan, Other
+* variety, una de las siguientes opciones: Bourbon, Catuai, Caturra, Typica, Other
+* aroma, permite seleccionar un valor flotante entre 0 y 10
+* aftertaste, idem aroma
+* acidity, idem aroma
+* body, idem aroma
+* balance, idem aroma
+* moisture, permite seleccionar un valor flotante entre 0 y 1
+
+3. Por defecto country=Brazil, variety=Bourbon y los demás campos están vacíos. Se controla antes de realizar la predicción que se hayan completado todos los campos.
+
+4. Para que se imprima el resultado se debe pulsar el botón PREDICT, siendo las opciones de respuesta dos (en función del resultado de la predicción):
+
+* Es un café de especialidad.
+* No es un café de especialidad.
+
+
+Link de la app: 
--- src/app.py
+++ src/app.py
@@ -13,8 +13,6 @@ loaded_model = pickle.load(open('models/coffee_model.pkl', 'rb'))
 def index():
     return render_template('index.html')
 
-# en flask app.route se debe escribir toda la palabra string
-
 #prediction function
 def ValuePredictor(to_predict_list):
     cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
@@ -22,50 +20,19 @@ def ValuePredictor(to_predict_list):
     result = loaded_model.predict(to_predict)
     return result.tolist()[0]
 
-
 @app.route('/result/', methods = ['POST'])
 def result():
-    #print(request.form.values())
-    #to_predict_list = request.form.values.to_dict()
-    #to_predict_list=list(to_predict_list.values())
-    #to_predict_list = list(map(int, to_predict_list))
     to_predict_list = [x for x in request.form.values()]
     print(to_predict_list)
     result = ValuePredictor(to_predict_list)
     print(result)
 
     if result == 'Yes':
-        #prediction = jsonify(message = 'Yes it is a specialty coffee')
         prediction = 'Yes it is a specialty coffee'
     else:
-        #prediction = jsonify(message = 'It is not a specialty coffee')
         prediction = 'It is not a specialty coffee'
-    print(prediction)       
-    return render_template("index.html", prediction)
 
-#@app.route('/<string:country>/<string:variety>/<float:aroma>/<float:aftertaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>')
-#def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture):
-#    cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
-#    data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
-#    posted = pd.DataFrame(np.array(data).reshape(1,8), columns = cols)
-#    # se predice con datos creados
-#    result = loaded_model.predict(posted)
-#    # salida a mostrar
-#    text_result = result.tolist()[0]
-#    if text_result == 'Yes':
-#        return jsonify(message = 'Si es un cafe de especialidad'), 200 # 200 es el código de error
-#    else:
-#        return jsonify(message = 'No es un cafe de especialidad'), 200
+    return render_template("index.html", prediction = prediction)
 
 if __name__ == '__main__':
     app.run(debug = True, host = '127.0.0.1', port = 5000)
-
-## prueba para si: Guatemala/Bourbon/7.83/7.67/7.33/7.67/7.67/0.11
-# ## prueba para no: Other/Other/7.25/6.83/7.17/7.00/7.17/0.11
-
-#### ejecutar en terminal en dos líneas distintas:
-#export FLASK_APP=src/app.py
-#export FLASK_ENV=development
-#run flask
-
-### https://nightlycommit.github.io/twing/language-reference/tags/extends.html
